6c843356abb69c875bc9d7f0ac384f782b560f1b
ivnmrtk <ivnmrtk@gmail.com>
2021-03-15 19:22:34 +0700

    initial commit

---
+++ src/main/java/com/github/ivnmrtk/transactionsvalidationservice/TransactionsValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.ivnmrtk.transactionsvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsValidationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsValidationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ivnmrtk/transactionsvalidationservice/TransactionsValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ivnmrtk.transactionsvalidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsValidationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
